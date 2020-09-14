d087710305a4de0c8ef83c0a2c0726f13ca05d43
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2020-09-15 05:23:53 +0900

    「投稿確認」の機能を実装完了

--- app/controllers/tclones_controller.rb
+++ app/controllers/tclones_controller.rb
@@ -7,15 +7,24 @@ class TclonesController < ApplicationController
   def create
     @tclone = Tclone.new(tclone_params)
     @tclone.save
+    redirect_to tclones_path, notice: "ツイートしました"
   end
 
   def index
     @tclones = Tclone.all
   end
 
+  def confirm
+    @tclone = Tclone.new(tclone_params)
+  end
+
   private
   def tclone_params
     params.require(:tclone).permit(:content)
+  end
+
+  def set_tclone
+    @tclone = Tclone.find(params[:id])
   end
 
 end
--- app/helpers/tclones_helper.rb
+++ app/helpers/tclones_helper.rb
@@ -1,2 +1,9 @@
 module TclonesHelper
+  def choose_new_or_edit
+    if action_name == 'new'
+      confirm_tclones_path
+    elsif action_name == 'edit'
+      edit_tclone_path
+    end
+  end
 end
